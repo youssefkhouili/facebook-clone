a6e950d94110608fc909c8bef53565be95814a5d
unknown <youssef.khouili10@gmail.com>
2020-11-16 12:16:57 +0100

    Refactoring api routes

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->group(function () {
 
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    // Route::get('posts', 'PostController@index');
+    // Route::post('posts', 'PostController@store');
 
-    Route::get('posts', 'PostController@index');
-    Route::post('posts', 'PostController@store');
+    Route::apiResources([
+        'posts' => 'PostController',
+        'users' => 'UserController'
+    ]);
 });
